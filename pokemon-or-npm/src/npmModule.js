0af1e78baf3cb5558d9c1af0176c11de41f4020c
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-09-28 16:55:46 -0300

    organizando e adicionando .json na url

--- pokemon-or-npm/src/npmModule.js
+++ pokemon-or-npm/src/npmModule.js
@@ -2,9 +2,10 @@ const fetch = require('node-fetch');
 
 
 const getPackageNpm = async (name) => {
-    const url = `https://pokeapi.co/api/v2/${name}`
+    const url = `https://pokeapi.co/api/v2/${name}`;
     const response = await fetch(url)
-    return response;
+    const data = response.json();
+    return data;
 }
 
 module.exports = getPackageNpm;
